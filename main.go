75d664d7174804201c895a7fbbe1413ec9e177c1
unknown <alex.chandra.wijoyo@gmail.com>
2018-04-02 20:03:15 +0700

    testing math

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import "fmt"
+import "math"
+
+const s string = "constant"
 
 func main() {
    fmt.Println("Hello, World!")
@@ -11,5 +14,33 @@ func main() {
    fmt.Println(true && true)
    fmt.Println(true || false)
    fmt.Println(!true)
+   fmt.Println("----------------------------------------------")
 
+var a = "initial"
+fmt.Println(a)
+
+var b, c int = 1, 2
+fmt.Println(b, c)
+
+var d = true
+fmt.Println(d)
+
+var e int
+fmt.Println(e)
+
+f:= "Short"
+fmt.Println(f)
+//The := syntax is shorthand for declaring and initializing a variable, e.g. for var f string = "short" in this case.
+
+fmt.Println("----------------------------------------------")
+fmt.Println(s)
+
+const n = 500000000
+
+const g = 3e20 / n
+fmt.Println(g)
+
+fmt.Println(int64(g))
+
+fmt.Println(math.Sin(n))
 }
